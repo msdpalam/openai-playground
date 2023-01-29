c7cbf0c8a2d8985d84d9469b577c684b803c9c26
Meer Alam <meeral@microsoft.com>
2023-01-29 17:56:47 -0600

    Update open-ai-classify.py

--- open-ai-classify.py
+++ open-ai-classify.py
@@ -9,7 +9,8 @@ RESOURCE_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
 
 openai.api_type = "azure"
 openai.api_base = RESOURCE_ENDPOINT
-openai.api_version = "2022-06-01-preview"
+openai.api_version = "2022-12-01" # this is the latest version as of 1/29/2023, check the latest version here: https://docs.microsoft.com/en-us/azure/cognitive-services/openai/reference-apis. 
+                                  # ou can also look at the Azure OpenAI Playground and check the sample code for the current version of your deployment
 openai.api_key = API_KEY
 
 response = openai.Completion.create(
